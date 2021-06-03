ba95ec41ac8194883455459fa7e2039763a742d0
Jie Bo <tijiebo@hotmail.com>
2021-06-04 02:10:40 +0800

    Updated Header paddings/margins

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ fontawesome.library.add(faSquare)
 export default function Header() {
     return (
         <Grid className={"center-container"} container justify={"center"} alignItems={"center"}
-              style={{height: "112px"}}>
+              style={{height: "96px"}}>
             <Grid container xs={12} sm={6} direction={"row"} justify={"center"} alignItems={"center"}>
                 <FontAwesomeIcon
                     icon="viruses" color={"#333333"} mask={["fas", "map"]} size={"3x"} transform={"shrink-6"}
@@ -19,12 +19,12 @@ export default function Header() {
                     onClick={() => {
                         window.location.reload()
                     }}/>
-                <h3 style={{paddingLeft: "4px"}} onClick={() => {
+                <h3 style={{margin: "8px 0px", paddingLeft: "4px"}} onClick={() => {
                     window.location.reload()
                 }}>covidwentthere</h3>
             </Grid>
             <Grid container xs={12} sm={6} justify={"center"} alignItems={"center"}>
-                <h3 onClick={() => {
+                <h3 style={{margin: "0px"}} onClick={() => {
                     window.location.assign("https://www.moh.gov.sg/news-highlights")
                 }}>Data obtained from MOH
                 </h3>
