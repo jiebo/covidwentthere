b5bcffdecc07880e72fc7294a7ef1ed41fca14f2
Jie Bo <tijiebo@hotmail.com>
2021-07-01 00:49:35 +0800

    Final update. Hardcoded date since it's the last

--- src/caa/CAA.js
+++ src/caa/CAA.js
@@ -1,4 +1,4 @@
-import {Box, Grid, Grow, IconButton, Paper, Typography} from "@material-ui/core";
+import {Box, Grid, Grow, IconButton, Link, Paper, Typography} from "@material-ui/core";
 import React, {useState} from "react";
 import CloseIcon from "@material-ui/icons/Close";
 
@@ -11,7 +11,7 @@ export default function CAA(params) {
     return (
         <Grid className={"caa-container"} container direction={"row-reverse"} justify={"center"}
               alignItems={"center"}
-              style={{width: "240px"}}>
+              style={{width: "280px"}}>
             <Grow in={display && params.show && params.caa !== 0 && params.zoom <= 12}>
                 <Paper square elevation={1} className={"caa-content"}>
                     <Grid container direction={"row"} style={{height: "100%", padding: "8px"}}
@@ -20,8 +20,9 @@ export default function CAA(params) {
                             <Box>
                                 <Typography variant={"subtitle1"}>Where <em>covid</em> visited</Typography>
                                 <Typography variant={"subtitle2"} style={{marginTop: "4px", marginBottom: "4px", lineHeight: "150%"}}>
-                                    Updated as of {caa}
+                                    Final update as of {caa}
                                 </Typography>
+                                <Typography variant={"caption"}><Link color={"secondary"} target={"_blank"} href={"https://www.straitstimes.com/singapore/health/moh-to-stop-giving-details-of-covid-19-community-cases"}>Data no longer available</Link></Typography>
                             </Box>
                         </Grid>
                         <Grid item xs={1}>
--- src/datecarousel/DateCarousel.js
+++ src/datecarousel/DateCarousel.js
@@ -5,8 +5,6 @@ import {Grid, IconButton, Paper, Typography} from "@material-ui/core";
 import {ChevronLeft, ChevronRight} from "@material-ui/icons";
 import useWindowDimensions from "../hooks/WindowDimensions";
 
-let dateFormat = require("dateformat");
-
 export default function DateCarousel(params) {
     const [index, setIndex] = React.useState(params.data.length - 1)
     const {width} = useWindowDimensions()
@@ -19,18 +17,12 @@ export default function DateCarousel(params) {
     const reset = () => {
         setIndex(14)
     };
-
-    const data = params.data ? params.data : []
-    const caa = new Date(params.caa)
     const items = []
-    for (let index in data) {
-        let date = new Date().setDate(caa.getDate() - index)
-        items.push(dateFormat(date, "d mmm"))
+    for (let i = 29; i >= 15; i--) {
+        items.push("" + i + " Jun")
     }
     items.reverse()
-    if (items.length >= 15) {
-        items[14] = items[14] ? "Past 2 weeks" : null
-    }
+    items[14] = items[14] ? "15 Jun - 28 Jun" : null
 
     return (
         <Grid className={"slider-container"} container direction={"row-reverse"} justify={"center"}
@@ -45,39 +37,39 @@ export default function DateCarousel(params) {
                     </IconButton>
                 </Paper>
             </Grid>
-                <Paper square elevation={1} className={"slider-content"}>
-                    <Grid container direction={"row"} style={{height: "100%"}}
-                          justify={"center"} alignItems={"center"}>
-                        <Grid item xs={12}>
-                            <Carousel
-                                className={"slider-content"}
-                                autoPlay={false}
-                                animation={"slide"}
-                                indicators={false}
-                                index={index >= 0 ? index : 14}
-                                navButtonsAlwaysVisible={true}
-                                onChange={(index, active) => {
-                                    params?.updateDisplay(index)
-                                    setIndex(index)
-                                }}
-                                navButtonsProps={{
-                                    style: {
-                                        padding: "4px",
-                                        margin: "4px 0px",
-                                        color: "#333",
-                                        background: "transparent"
-                                    }
-                                }}
-                                navButtonsWrapperProps={{
-                                    style: {}
-                                }}
-                                NextIcon={<ChevronRight/>}
-                                PrevIcon={<ChevronLeft/>}>
-                                {items.map((item, i) => <Item key={i} date={item}/>)}
-                            </Carousel>
-                        </Grid>
+            <Paper square elevation={1} className={"slider-content"}>
+                <Grid container direction={"row"} style={{height: "100%"}}
+                      justify={"center"} alignItems={"center"}>
+                    <Grid item xs={12}>
+                        <Carousel
+                            className={"slider-content"}
+                            autoPlay={false}
+                            animation={"slide"}
+                            indicators={false}
+                            index={index >= 0 ? index : 14}
+                            navButtonsAlwaysVisible={true}
+                            onChange={(index, active) => {
+                                params?.updateDisplay(index)
+                                setIndex(index)
+                            }}
+                            navButtonsProps={{
+                                style: {
+                                    padding: "4px",
+                                    margin: "4px 0px",
+                                    color: "#333",
+                                    background: "transparent"
+                                }
+                            }}
+                            navButtonsWrapperProps={{
+                                style: {}
+                            }}
+                            NextIcon={<ChevronRight/>}
+                            PrevIcon={<ChevronLeft/>}>
+                            {items.map((item, i) => <Item key={i} date={item}/>)}
+                        </Carousel>
                     </Grid>
-                </Paper>
+                </Grid>
+            </Paper>
         </Grid>
     )
 }
